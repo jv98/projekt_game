1640b82346a7088094cf8d0278ab22d952b1de0a
jv98 <jacob@vahlen.se>
2020-10-27 09:18:52 +0100

    this is a test

---
+++ script.js
@@ -0,0 +1,33 @@
+
+alert('hello traveler big disater has happen and you need solve this big problem')
+alert('you see man in corner of the one of huse it look like it was runw donw')
+
+const talk = prompt("do what talk too the man");
+x = speak (talk);
+do {
+function speak(talk) {
+        
+  
+        
+  
+    if (talk === 'no'){
+        alert ('should talk to the man')
+    }
+    else if  (talk === 'yes') {
+        alert('hello there this wolrd whent too ruine the last war to revert all of this you need to find the time machin')
+        alert('to find the time machin you need finde som part to repair machin but the part is scaterd all over the place')
+    }
+    else{
+        alert('hint say yes')
+    }   
+
+}
+} while (talk === 'yes');
+
+const house1 = prompt("do what go in to house");
+y= partrom(house1);
+  function partrom(house1) {
+      if (house1 === 'yes'){
+          alert ('you walk in too huse next to man you can see onley see darknes')
+      }
+  }
