6da407ffb08616b0ee14fd2d19bd7a747ef199e9
jv98 <jacob@vahlen.se>
2020-10-27 14:53:47 +0100

    fick do while loop att fuka

--- script.js
+++ script.js
@@ -2,34 +2,38 @@
 alert('hello traveler big disater has happen and you need solve this big problem')
 alert('you see man in corner of the one of huse it look like it was runw donw')
 
-const talk = prompt("do what talk too the man");
-x = speak (talk);
+
+x = speak ();
 //ska gör en do while som ska gör en koden tills person skriver yes
 
-function speak(talk) {
-        
+function speak() {
+        let talk
     do {
-
-    if (talk === 'no'){
-        alert ('should talk to the man')
-    }
-
-    else if  (talk === 'yes') {
-        alert('hello there this wolrd whent too ruine the last war to revert all of this you need to find the time machin')
-        alert('to find the time machin you need finde som part to repair machin but the part is scaterd all over the place')
-    }
-    else{
-        alert('hint say yes')
-    }   
-    } while (talk = 'yes');
+       talk = prompt("do what talk too the man");
+        if  (talk === 'yes') {
+            alert('hello there this wolrd whent too ruine the last war to revert all of this you need to find the time machin')
+            alert('to find the time machin you need finde som part to repair machin but the part is scaterd all over the place')
+        }
+        else if (talk === 'no'){
+            alert ('should talk to the man')
+        }
+        else{
+            alert('hint say yes')
+        }   
+    } while (talk !== 'yes') ;
 
 }
 const walk1 = prompt("do what go to house");
+y= partrom(house1);
+
 const walk2 = prompt("text holder")
+
 const walk3 = prompt("text holder")
+
+
 if (walk1 === 'yes') {
 const house1 = prompt("do what go in to house");
-y= partrom(house1);
+
   function partrom(house1) {
       if (house1 === 'yes'){
           alert ('you walk in too huse next to man you can see onley see darknes')
