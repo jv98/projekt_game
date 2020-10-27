b9ef73dddcd5223e6ee9c1e54a197aea5a6eb3cc
jv98 <jacob@vahlen.se>
2020-10-27 18:32:03 +0100

    fixade så att vallen kunde loppa runt

--- script.js
+++ script.js
@@ -3,46 +3,56 @@ alert('hello traveler big disater has happen and you need solve this big problem
 alert('you see man in corner of the one of huse it look like it was runw donw')
 
 
-x = speak ();
+x = speak();
 //ska gör en do while som ska gör en koden tills person skriver yes
 
 function speak() {
-        let talk
+    let talk
     do {
-       talk = prompt("do what talk too the man");
-        if  (talk === 'yes') {
+
+        talk = prompt("do what talk too the man");
+        talk = talk.toLowerCase();
+        if (talk === 'yes') {
             alert('hello there this wolrd whent too ruine the last war to revert all of this you need to find the time machin')
             alert('to find the time machin you need finde som part to repair machin but the part is scaterd all over the place')
         }
-        else if (talk === 'no'){
-            alert ('should talk to the man')
+        else if (talk === 'no') {
+            alert('should talk to the man')
         }
-        else{
+        else {
             alert('hint say yes')
-        }   
-    } while (talk !== 'yes') ;
+        }
+    } while (talk !== 'yes');
 
 }
-const walk1 = prompt("do what go to house");
-y= partrom(house1);
 
-const walk2 = prompt("text holder")
+let walk1
+let walk2
+let walk3
+do {
+ 
+    const walk1 = prompt("do what go to house");
+ if ( walk1 === 'yes') {
+    
+    partrom();
+}
+const walk2 = prompt("text holder");
+ if (walk2 === 'yes') {
+    function place2(params) {
 
-const walk3 = prompt("text holder")
+    }
 
+}
+ const walk3 = prompt("text holder");
+ if (walk3 === 'yes') {
+    function name(params) {
 
-if (walk1 === 'yes') {
-const house1 = prompt("do what go in to house");
+    }
+}
+} while( walk1 !== 'yes'|| walk2 !== 'yes' || walk3 !== 'yes');
 
-  function partrom(house1) {
-      if (house1 === 'yes'){
-          alert ('you walk in too huse next to man you can see onley see darknes')
-      }
-  }
-    if (walk2 === 'yes') {
-      
-  }
-  else if (walk3 ==='yes') {
-      
-  }
+function partrom() {
+        alert("you ennter");
+        alert('you walk in too huse next to man you can see onley see darknes')
+    
 }
