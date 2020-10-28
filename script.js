8bc5628f1997251b08976d1ec385dd2c3e987852
jv98 <jacob@vahlen.se>
2020-10-28 17:38:12 +0100

    la till ny saker

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 
-alert('hello traveler big disater has happen and you need solve this big problem')
-alert('you see man in corner of the one of huse it look like it was runw donw')
-
-
+let lever = false;
+let fusitoncore = false;
+let nuclerpower = false;
 x = speak();
+
+
 //ska gör en do while som ska gör en koden tills person skriver yes
 
+alert('hello traveler big disater has happen and you need solve this big problem')
+alert('you see man in corner of the one of huse it look like it was runw donw')
 function speak() {
     let talk
     do {
-
+// spara vädert i en varibale och och gör en elese if stamtet och skriv att spelet är sluts
         talk = prompt("do what talk too the man");
         talk = talk.toLowerCase();
         if (talk === 'yes') {
@@ -25,10 +28,11 @@ function speak() {
     } while (talk !== 'yes');
 
 }
-
 let walk1
 let walk2
 let walk3
+let walk4
+
 do {
  
     const walk1 = prompt("do what go to house");
@@ -38,21 +42,60 @@ do {
 }
 const walk2 = prompt("text holder");
  if (walk2 === 'yes') {
-    function place2(params) {
-
-    }
-
+    place2();
 }
  const walk3 = prompt("text holder");
  if (walk3 === 'yes') {
-    function name(params) {
+    place3();
+}
 
-    }
+ if (a, b ,c && walk4 === 'yes') {
+    const walk4 = prompt("text holder")
+    endroom();
 }
 } while( walk1 !== 'yes'|| walk2 !== 'yes' || walk3 !== 'yes');
 
+
+
 function partrom() {
         alert("you ennter");
-        alert('you walk in too huse next to man you can see onley see darknes')
+         alert('you walk in too huse next to man you can see onley see darknes')
+      if (walk1 === 'yes') {
+      //palace wer walk in too
+      } 
+      else if (walk1) {
+          // you look arouound the rom
+      }
+      else if (walk1) {
+          //
+    }
+    else if (walk1){
+          //
+    }
+    const partrom = prompt("do what take upp the item")
+    if (walk1 === 'take') {
+        item1 = "lever";
+        
+    }  
+        else (walk1 === 'no') 
+            alert ('you wake back to text holder')
+        
+} 
+function place2() {
+   if (condition) {
+       
+   } else if(walk2){
+       
+   }
+   else if(walk2){
+
+   }
+}
+
+function place3() {
+
+}
+
+function endroom() {
     
 }
